b25dc4691fd6dc004af51d6ce475392354db8644
karinsudro <109920062+karinsudro@users.noreply.github.com>
2022-11-02 20:35:10 -0300

    Add files via upload

---
+++ src/navbaradmin/logoapadmin/logoapadmin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logoapadmin',
+  templateUrl: './logoapadmin.component.html',
+  styleUrls: ['./logoapadmin.component.css']
+})
+export class LogoapadminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
